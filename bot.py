d562e98b2a0658a2b6f9cd364b43804f50250554
SoHectic <46034067+SoHectic@users.noreply.github.com>
2020-07-20 15:32:43 -0400

    Initial Commit

---
+++ bot.py
@@ -0,0 +1,22 @@
+import discord
+import random
+import os
+import shutil
+from discord.ext import commands, tasks
+from discord.utils import get
+
+client = commands.Bot(command_prefix = '-')
+client.remove_command('help')
+
+@client.event
+async def on_ready():
+    print('Bot is Ready!')
+    await client.change_presence(status=discord.Status.online, activity=discord.Game('Developed By Hectic'))
+
+@client.event
+async def on_message(message):
+    if message.channel.id == 734819118708162580:
+        target_channel = client.get_channel(529038351312093185)
+        await target_channel.send(message.content)
+
+client.run(TOKEN)
