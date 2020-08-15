9e65a57e4cb7eed9780f9be3b65a6267125c8f8d
SoHectic <46034067+SoHectic@users.noreply.github.com>
2020-08-15 17:09:13 -0400

    Removed some code breaking bot

--- bot.py
+++ bot.py
@@ -18,6 +18,5 @@ async def on_message(message):
     if message.channel.id == client.get_channel(735165097021800571):
         target_channel = client.get_channel(529039796404879370)
         await target_channel.send(message.content)
-    else:
-        await client.process_commands(message)
+
 client.run(os.environ['TOKEN'])
