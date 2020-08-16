f4a822049c60c66e8923ba109e394feae68798c3
SoHectic <46034067+SoHectic@users.noreply.github.com>
2020-08-16 10:21:24 -0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -20,4 +20,4 @@ async def on_message(message):
         await target_channel.send(message.content)
     else:
         await client.process_commands(message)
-client.run('NzM0ODAzMTcwNDYyNjYyNjc4.XxXAxw.8J_X12PalRgaBM8C3XA8LPkxhYg')
+client.run(os.environ['token'])
